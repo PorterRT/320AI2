f0711a0615b0c76dc9d5322083334869266956a9
PorterRT <91439082+PorterRT@users.noreply.github.com>
2023-11-07 00:47:53 +0000

    super late nerual networks my bad

--- app.py
+++ app.py
@@ -1,25 +1,56 @@
 import openai
 import os
 import chainlit as cl
+import glob
 
 # Configure OpenAI API Key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-@cl.on_message  # this function will be called every time a user inputs a message in the UI
-async def main(message: str):
-    
-    # Fetch response from OpenAI GPT-4.0 with pirate instruction
+# Define the folder containing PNG data
+png_data_folder = "png_data_folder/"
+
+async def bot_response(name: str, system_instruction_path: str, lesson_content_path: str, message: str):
+    with open(system_instruction_path, "r") as system_file:
+        system_instruction = system_file.read()
+
+    with open(lesson_content_path, "r") as lesson_file:
+        lesson_content = lesson_file.read()
+
     chat_completion = openai.ChatCompletion.create(
         model="gpt-4", messages=[
-            {"role": "system", "content": "speak like darth vader"},
-            {"role": "user", "content": message}
+            {"role": "system", "content": system_instruction},
+            {"role": "user", "content": message},
+            {"role": "assistant", "content": lesson_content}
         ]
     )
-    
+
     response_message = chat_completion["choices"][0]["message"]["content"]
     
-    # this is an intermediate step (you can modify this as per your requirements)
-    await cl.Message(author="Vader", content=f" {response_message}", indent=1).send()
+    # Process PNG files in the specified folder
+    png_files = glob.glob(os.path.join(png_data_folder, "*.png"))
+    for png_file in png_files:
+        await process_png_file(png_file)
+    
+    await cl.Message(author=name, content=f"{name} says: {response_message}", indent=1).send()
 
-    # send back the final answer (or another formatted response)
-    await cl.Message(content=f"Vader says: {response_message}").send()
+    # Return response message for further analysis
+    return response_message
+
+async def process_png_file(png_file):
+    # Implement your logic to process each PNG file here
+    pass
+
+@cl.on_message
+async def main(message: str):
+    response = await bot_response("Vader", "system_instruction_vader.txt", "lesson_vader.ipynb", message)
+    
+    if "##SkywalkerSection" in response:
+        response = await bot_response("Skywalker", "system_instruction_skywalker.txt", "lesson_skywalker.ipynb", message)
+    elif "##KenobiSection" in response:
+        response = await bot_response("Kenobi", "system_instruction_kenobi.txt", "lesson_kenobi.ipynb", message)
+    elif "##LukeSection" in response:
+        response = await bot_response("Luke", "system_instruction_luke.txt", "lesson_luke.ipynb", message)
+
+    # If Skywalker or Kenobi have done their part, let Vader take over again
+    if "##VaderReturn" in response:
+        await bot_response("Vader", "system_instruction_vader.txt", "lesson_vader.ipynb", message)
